d0e79956964ece60604bf06bb6abad213e2b6f46
JustMrArgus <argusprogramming@protonmail.com>
2025-10-13 23:56:36 +0300

    Add healthcheck resposne content

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(express.json());
 
 app.get("/healthcheck", (req, res) => {
-  res.status(200).json({});
+  res.status(200).json({
+    status: "ok",
+    date: new Date().toISOString(),
+  });
 });
 
 module.exports = app;
