49dbd8b132d4ab38cbec1d3a7df186fc29b7ac9b
JustMrArgus <argusprogramming@protonmail.com>
2025-10-13 23:53:48 +0300

    Add /healthcheck endpoint

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/healthcheck", (req, res) => {
+  res.status(200).json({});
+});
+
 module.exports = app;
